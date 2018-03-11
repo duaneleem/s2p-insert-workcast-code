ad1285f576d6e9fb74dec998c68f08120cb2f315
Duane Leem <Duane@LeemFamily.com>
2018-03-11 15:09:56 -0700

    Changed to CloudFront path.

--- pages/template-workcast.php
+++ pages/template-workcast.php
@@ -6,6 +6,6 @@
 
 get_header(); ?>
 
-<iframe src="<?php echo plugin_dir_url( __FILE__ ) . '../assets/html/workcast.html' ?>" class="workcast-iframe">Your browser is not supported.</iframe>
+<iframe src="https://d1qlu92qlflogk.cloudfront.net/workcast.html" class="workcast-iframe">Your browser is not supported.</iframe>
 
 <?php get_footer();
