54cdbd9c5e961aba22c6130423d83a29f777ab44
Ubuntu <ubuntu@ip-172-26-8-76.us-west-2.compute.internal>
2018-03-15 18:45:19 +0000

    Added section container class.

--- pages/template-workcast.php
+++ pages/template-workcast.php
@@ -12,12 +12,12 @@ $objThePost = get_post();
 get_header(); ?>
 
 <!-- TODO: WP Editor Contents -->
-<section id="workcast-wp-main">
+<section class="container" id="workcast-wp-main">
   <?php echo apply_filters('the_content', $objThePost->post_content); ?>
 </section>
 
 <!-- Modify the S3 files to change the look of this page. -->
-<section id="section-workcast-iframe">
+<section class="container" id="section-workcast-iframe">
   <!-- Does NOT work. Reverting to CloudFront alternative. -->
   <!-- <iframe src="<?php echo plugin_dir_url( __FILE__ ) . '../assets/html/workcast.html' ?>" class="workcast-iframe">Your browser is not supported.</iframe> -->
   <iframe src="https://d1qlu92qlflogk.cloudfront.net/workcast/workcast.html" class="workcast-iframe">Your browser is not supported.</iframe>
