b999c490a45532a1221189322a58bb0b65a0960e
Duane Leem <Duane@LeemFamily.com>
2018-03-15 11:38:53 -0700

    Added styles.

--- pages/template-workcast.php
+++ pages/template-workcast.php
@@ -12,12 +12,15 @@ $objThePost = get_post();
 get_header(); ?>
 
 <!-- TODO: WP Editor Contents -->
-<?php echo apply_filters('the_content', $objThePost->post_content); ?>
-
-<!-- Does NOT work. Reverting to CloudFront alternative. -->
-<!-- <iframe src="<?php echo plugin_dir_url( __FILE__ ) . '../assets/html/workcast.html' ?>" class="workcast-iframe">Your browser is not supported.</iframe> -->
+<section id="workcast-main">
+  <?php echo apply_filters('the_content', $objThePost->post_content); ?><?php echo apply_filters('the_content', $objThePost->post_content); ?>
+</section>
 
 <!-- Modify the S3 files to change the look of this page. -->
-<iframe src="https://d1qlu92qlflogk.cloudfront.net/workcast/workcast.html" class="workcast-iframe">Your browser is not supported.</iframe>
+<section id="section-workcast-iframe">
+  <!-- Does NOT work. Reverting to CloudFront alternative. -->
+  <!-- <iframe src="<?php echo plugin_dir_url( __FILE__ ) . '../assets/html/workcast.html' ?>" class="workcast-iframe">Your browser is not supported.</iframe> -->
+  <iframe src="https://d1qlu92qlflogk.cloudfront.net/workcast/workcast.html" class="workcast-iframe">Your browser is not supported.</iframe>
+</section>
   
 <?php get_footer();
