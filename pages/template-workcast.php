270bc967a0053a57c334e265aa721852e84ef0f8
Duane Leem <Duane@LeemFamily.com>
2018-03-15 11:08:52 -0700

    Changed location where HTML is pointed to at S3.

--- pages/template-workcast.php
+++ pages/template-workcast.php
@@ -9,6 +9,7 @@ get_header(); ?>
 <!-- Does NOT work. Reverting to CloudFront alternative. -->
 <!-- <iframe src="<?php echo plugin_dir_url( __FILE__ ) . '../assets/html/workcast.html' ?>" class="workcast-iframe">Your browser is not supported.</iframe> -->
 
-<iframe src="https://d1qlu92qlflogk.cloudfront.net/workcast.html" class="workcast-iframe">Your browser is not supported.</iframe>
+<!-- Modify the S3 files to change the look of this page. -->
+<iframe src="https://d1qlu92qlflogk.cloudfront.net/workcast/workcast.html" class="workcast-iframe">Your browser is not supported.</iframe>
   
 <?php get_footer();
